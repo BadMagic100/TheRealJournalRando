0f5d448f4e862c1331795419ee2100dbb527e38e
Sean Dempsey <dempsey.sean@outlook.com>
2025-11-09 23:12:06 -0800

    Migrate to DataDrivenConstants

--- TheRealJournalRando/Data/GeneratedConsts.cs
+++ TheRealJournalRando/Data/GeneratedConsts.cs
@@ -1,13 +1,14 @@
-﻿using RandoConstantGenerators;
+﻿using DataDrivenConstants.Marker;
 
 namespace TheRealJournalRando.Data.Generated
 {
-    [GenerateJsonConsts("$[*].icName", "journalData.json")]
+    [JsonData("$[*].icName", "**/journalData.json")]
+    [ReplacementRule("'", "")]
     public static partial class EnemyNames
     {
         public const string Void_Idol_Prefix = "Void_Idol_";
     }
 
-    [GenerateJsonConsts("$[*].pdName", "journalData.json")]
+    [JsonData("$[*].pdName", "**/journalData.json")]
     public static partial class EnemyPdNames { }
 }
